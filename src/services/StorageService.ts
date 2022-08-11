bfc4c980546e7d527130d167f9e359def85c6f1a
InfantAjayVenus <InfantAjay.Venus@Cognizant.com>
2022-08-11 09:17:44 +0530

    [store] fix: add logic to restore broken tasks

--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -1,14 +1,29 @@
+import { DEFAULT_SECTION_ID } from "../features/tasks/sectionSlice";
+
 const KEY = "PDTR_STORE";
 export function loadState():any {
   try {
     const serializedState = localStorage.getItem(KEY);
     if (!serializedState) return undefined;
-    
-    return JSON.parse(serializedState);
+    const storeData = JSON.parse(serializedState);
+    const validStore = getValidatStore(storeData);
+
+    return validStore;
   } catch (e) {
     console.log(e);
     return undefined;
   }
+}
+
+function getValidatStore(storeData: any) {
+  const {tasks, sections} = storeData;
+  const sectionIdList = sections.sectionsList.map(({id}) => id);
+  const validTasks = tasks.tasksList.map(task => {
+    if(sectionIdList.includes(task.sectionId)) return task;
+    return {...task, sectionId: DEFAULT_SECTION_ID}
+  })
+
+  return {sections, tasks: {...tasks, tasksList: validTasks}};
 }
 
 export async function saveState(state: any) {
