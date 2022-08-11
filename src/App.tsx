55845f5e1fda255c46d13221539786497eee2e47
InfantAjayVenus <InfantAjay.Venus@Cognizant.com>
2022-08-11 09:15:56 +0530

    [app] feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { MantineProvider } from "@mantine/core";
-import Home from './pages/Home';
+import { ActionIcon, MantineProvider, Tooltip } from "@mantine/core";
+import { BrowserRouter as Router, NavLink, Routes, Route } from 'react-router-dom'
+import Sections from './pages/Sections';
+import { Home, ListCheck } from "tabler-icons-react";
+
+import NavContainer from './pages/layout-components/NavContainer';
+import Tasks from "./pages/Tasks";
+// import NewItem from "./pages/NewItem";
 
 function App() {
   return (
@@ -9,7 +15,30 @@ function App() {
       withGlobalStyles
       withNormalizeCSS
     >
-      <Home />
+      <Router>
+        <NavContainer>
+          <Tooltip label={'All Tasks'}>
+            <NavLink to='/tasks'>
+              <ActionIcon><ListCheck /></ActionIcon>
+            </NavLink>
+          </Tooltip>
+          <Tooltip label={'Home'}>
+            <NavLink to='/'>
+              <ActionIcon><Home /></ActionIcon>
+            </NavLink>
+          </Tooltip>
+        </NavContainer>
+        <main>
+          <Routes>
+            <Route path="/" element={<Sections />} >
+            </Route>
+            <Route path="/tasks/:sectionId" element={<Tasks />} >
+            </Route>
+            <Route path="/tasks" element={<Tasks />} >
+            </Route>
+          </Routes>
+        </main>
+      </Router>
     </MantineProvider>
   );
 }
