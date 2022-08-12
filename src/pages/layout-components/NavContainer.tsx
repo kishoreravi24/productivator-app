b51d54462c19047982a5273d94a32fc38f553af7
InfantAjayVenus <InfantAjay.Venus@Cognizant.com>
2022-08-12 06:31:26 +0530

    [Nav] feat: add responsive styles

---
+++ src/pages/layout-components/NavContainer.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { createStyles } from '@mantine/core';
+
+const useContaienrStyles = createStyles((theme) => ({
+
+    'container': {
+        display: 'flex',
+        flexFlow: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        position: 'absolute',
+        width: '90%',
+        padding: '0.3rem 0',
+        background: theme.colors.dark[6],
+        boxShadow: theme.shadows.md,
+        borderRadius: theme.radius.md,
+        bottom: '1rem',
+        left: '5%',
+
+        'a': {
+            'button': {
+                height: '3rem',
+                width: '3rem',
+                color: theme.colors.dark[3]
+            },
+
+            '&.active': {
+                borderBottom: `0.2rem solid ${theme.colors[theme.colorScheme][0]}`,
+                'button': {
+                    color: theme.white
+                    
+                 }
+            }
+        },
+
+        [`@media (min-width: ${theme.breakpoints.sm}px)`]: {
+            width: '50%',
+            left: 'unset',
+            right: '5%'
+        },
+
+        [`@media (min-width: ${theme.breakpoints.md}px)`]: {
+            flexFlow: 'column-reverse',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            width: '3rem',
+            height: '100%',
+            borderRadius: '0',
+            left: '0',
+            top: '0',
+
+            'a': {
+                '&.active': {
+                    borderBottom: 'none',
+                    borderLeft: `0.2rem solid ${theme.colors[theme.colorScheme][0]}`
+                }
+            }
+        }
+
+    }
+}))
+
+const NavContainer = ({ children }) => {
+    const { classes } = useContaienrStyles();
+    return (
+        <nav className={classes.container}>
+            {children}
+        </nav>
+    )
+}
+
+export default NavContainer;
